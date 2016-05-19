ea2636b30e0c13b451b26535244ef79a329eb222
Karthik <karthik.kasala@gmail.com>
2016-05-19 00:59:54 -0400

    using unittest module

--- functional_tests.py
+++ functional_tests.py
@@ -1,17 +1,26 @@
 from selenium import webdriver
+import unittest
 
+class NewVisitorTest(unittest.TestCase):
 
-browser = webdriver.Firefox()
-# Edith has heard about a new online to-do app. She goes to check out its
-# homepage
-browser.get('http://localhost:8000')
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-# She notices the page title/header mention to-do lists
-assert 'To-Do' in browser.title
+    def tearDown(self):
+        self.browser.quit()
 
-# She is prompted to enter a to-do item
-# She does other things the author is too tired to currently write about
-# Eventually, after entering in things for her list, she checks the URL later
-# and sees her list still there
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith has heard about a new online to-do app. She goes to check out its
+        # homepage
+        browser.get('http://localhost:8000')
 
-browser.quit()
+        # She notices the page title/header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+        # She is prompted to enter a to-do item
+        # She does other things the author is too tired to currently write about
+        # Eventually, after entering in things for her list, she checks the URL later
+        # and sees her list still there
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
