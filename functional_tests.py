5aa865362cdb696649b88614e8276ae2f9ace056
Karthik <karthik.kasala@gmail.com>
2016-05-19 00:51:23 -0400

    changed browser.title to intentionally fail test

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,17 @@
 from selenium import webdriver
 
+
 browser = webdriver.Firefox()
+# Edith has heard about a new online to-do app. She goes to check out its
+# homepage
 browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+# She notices the page title/header mention to-do lists
+assert 'To-Do' in browser.title
+
+# She is prompted to enter a to-do item
+# She does other things the author is too tired to currently write about
+# Eventually, after entering in things for her list, she checks the URL later
+# and sees her list still there
+
+browser.quit()
